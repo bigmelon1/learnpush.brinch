0e7dd46ac2f8ce07287349813cc21445382eeb8a
Jun Woo Lee <cutemelon_@naver.com>
2022-07-25 16:28:01 +0900

    모바일용navbar1

---
+++ main.js
@@ -0,0 +1,11 @@
+let 네브바열림 = 0;
+
+function navbaronoff(){
+    if (document.getElementById('navbar-menu').style.display === 'none') {
+        //document.getElementById('navbar-menu').style.display = 'block';
+        네브바열림 = 1;
+    }else {
+        //document.getElementById('navbar-menu').style.display = 'none';
+        네브바열림 = 0;
+    }
+}
