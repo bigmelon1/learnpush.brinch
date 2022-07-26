d9576ec49d9d54c8d736b6af0cd16c6d030c358f
Jun Woo Lee <cutemelon_@naver.com>
2022-07-26 13:23:45 +0900

    navbarphone거의완성1

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 let 네브바열림 = 0;
 
+
+
+
+
 function navbaronoff(){
-    if (document.getElementById('navbar-menu').style.display === 'none') {
-        //document.getElementById('navbar-menu').style.display = 'block';
+    if (document.getElementById('navdrop').style.display === 'none') {        
+        document.getElementById('navdrop').style.display = 'block';
+        document.getElementById('navdrop').style.display = 'flex';
+        document.getElementById('navdrop-uldiv').style.display = 'block';
         네브바열림 = 1;
     }else {
-        //document.getElementById('navbar-menu').style.display = 'none';
+        document.getElementById('navdrop').style.display = 'none';
+        document.getElementById('navdrop-uldiv').style.display = 'none';
         네브바열림 = 0;
     }
 }
+
+
+//function navbaronoff(){
+//    if (document.getElementById('navbar-menu').style.display === 'none') {
+        //document.getElementById('navbar-menu').style.display = 'block';
+//        네브바열림 = 1;
+//    }else {
+        //document.getElementById('navbar-menu').style.display = 'none';
+//        네브바열림 = 0;
+//    }
+//}
