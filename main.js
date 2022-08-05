3f5adcb0a867af595e6146856c827253681c1eb3
Jun Woo Lee <cutemelon_@naver.com>
2022-08-05 15:18:46 +0900

    main 완성!!

--- main.js
+++ main.js
@@ -16,14 +16,3 @@ function navbaronoff(){
         네브바열림 = 0;
     }
 }
-
-
-//function navbaronoff(){
-//    if (document.getElementById('navbar-menu').style.display === 'none') {
-        //document.getElementById('navbar-menu').style.display = 'block';
-//        네브바열림 = 1;
-//    }else {
-        //document.getElementById('navbar-menu').style.display = 'none';
-//        네브바열림 = 0;
-//    }
-//}
